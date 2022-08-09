cc216446a2eba245d29fa7329cfd572e1e89e108
Jaymie Batoon <jaymiebatoon@Jaymies-MacBook-Pro-2.local>
2022-08-08 21:50:45 -0400

    created user_database and seeded initial data.

--- drf_jwt_backend/local_settings.py
+++ drf_jwt_backend/local_settings.py
@@ -4,7 +4,7 @@ SECRET_KEY = 'bk5=h+i5o_)s7#f53^4aj(ehc&()qc!1602(gd5p4c+-7u'
 DATABASES = {
     'default': {
         'ENGINE': 'mysql.connector.django',
-        'NAME': 'youtube_database',
+        'NAME': 'user_database',
         'USER': 'root',
         'PASSWORD': 'Addison2007!',
         'HOST': '127.0.0.1',

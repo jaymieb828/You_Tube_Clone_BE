069fac1dcbd4aee76c7b067eb69f7783bd0afac7
Jaymie Batoon <jaymiebatoon@Jaymies-MacBook-Pro-2.local>
2022-08-09 13:59:08 -0400

    Created and coded out urls.py and vies.py

---
+++ comments/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from comments import views
+
+urlpatterns = [
+    path('<str:id>/', views.get_all_comments),
+    path('', views.post_comment),
+    path('<int:pk>/update/', views.update_comment),
+]
